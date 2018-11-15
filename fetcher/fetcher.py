f42576606d3d71f4186754bbc054b115d94813c3
enricorotundo <enrico.rotundo@gmail.com>
2018-11-15 23:57:01 +0100

    fixed logging level

--- fetcher/fetcher.py
+++ fetcher/fetcher.py
@@ -12,7 +12,7 @@ logger = logging.getLogger('fetcher.py')
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
+ch.setLevel(logging.DEBUG)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
