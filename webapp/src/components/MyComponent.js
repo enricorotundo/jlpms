be488957bd3341bd1757f875bc43ee9e46c86ace
Giacomo Fornari <fornarigiacomo@gmail.com>
2018-11-18 12:46:56 +0100

    Add componentDidMount example

--- webapp/src/components/MyComponent.js
+++ webapp/src/components/MyComponent.js
@@ -3,11 +3,25 @@ import React, { Component, Fragment } from 'react';
 class MyComponent extends Component {
   constructor() {
     super();
+
+    this.state = {
+      title: 'My old title'
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        title: 'My new title'
+      })
+    }, 3000);
   }
 
   render() {
+    const { title } = this.state;
+
     return <Fragment>
-      <h2>MyComponent title</h2>
+      <h2>{ title }</h2>
       <p>MyComponent paragraph</p>
     </Fragment>;
   }
