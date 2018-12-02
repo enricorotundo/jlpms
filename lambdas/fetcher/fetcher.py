1cbba3a7cc9c197065b51470ab27f03f9b28bfa3
Enrico Rotundo <enrico.rotundo@hal24k.com>
2018-12-02 12:12:06 +0100

    logging filename instad of str

--- lambdas/fetcher/fetcher.py
+++ lambdas/fetcher/fetcher.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 from algoliasearch import algoliasearch
 
 # logging setup
-logger = logging.getLogger('fetcher.py')
+logger = logging.getLogger(__file__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch = logging.StreamHandler()
