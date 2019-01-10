d17d29b7dad99e2b5f53c8d0f95ea18e974a56c2
Giacomo Fornari <fornarigiacomo@gmail.com>
2019-01-10 22:43:34 +0100

    Add first working SSR for SEO

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
 
-var app = express();
+// ugly, but..
+const environmentToEnv = {
+  'production': 'prod',
+  'staging': 'stag',
+  'development': 'dev'
+}
+const env = require(`./env.${environmentToEnv[process.env.NODE_ENV]}.json`);
 
+const app = express();
+
+app.use(require('prerender-node').set('prerenderServiceUrl', env.PRERENDER_SERVICE_URL));
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.listen(8080, () => {
