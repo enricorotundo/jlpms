b3b2373e2b151e9abfdab9ba275e3dfb0b5f54ec
Giacomo Fornari <fornarigiacomo@gmail.com>
2019-03-17 20:02:51 +0100

    Add search bar

--- webapp/src/routes/Home.js
+++ webapp/src/routes/Home.js
@@ -1,11 +1,26 @@
 import React, { Component, Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import { InstantSearch, Hits, SearchBox, Highlight, Configure } from 'react-instantsearch-dom';
 import MostRecentPackages from '../components/MostRecentPackages';
 
 const styles = theme => ({
-
+  instantSearchContainer: {
+    padding: theme.spacing.unit
+  },
+  searchContainer: {
+    padding: theme.spacing.unit
+  }
 });
+
+function Hit({ hit }) {
+  return (<div style={{ marginTop: '10px' }}>
+    <span className="hit-name">
+      <Highlight attribute="full_name" hit={hit} />
+    </span>
+    <Highlight attribute="description" hit={hit} />
+  </div>);
+}
 
 class Home extends Component {
   constructor() {
@@ -16,10 +31,20 @@ class Home extends Component {
     const { classes } = this.props;
 
     return <Fragment>
-      <Grid container className={classes.tableContainer}>
-        <Grid item>
-          <MostRecentPackages />
-        </Grid>
+      <Grid container className={classes.instantSearchContainer}>
+        <InstantSearch
+          appId="7TYWZIEJIK"
+          apiKey="cc8135b4e1abb63101b50c0b5d606676"
+          indexName="dev_jlpms"
+        >
+        <Configure hitsPerPage={10} />
+          <Grid item className={classes.searchContainer}>
+            <SearchBox />
+          </Grid>
+          <Grid item className={classes.searchContainer}>
+            <Hits hitComponent={Hit} />
+          </Grid>
+        </InstantSearch>
       </Grid>
     </Fragment>;
   }
